ff7a2ff78dfa0ad042fb744601502fd8f5b9aef5
Subhra <subhra.bhuyan@merittrac.com>
2022-03-10 18:25:10 +0530

    dashboard ui

---
+++ resources/js/Components/MainMenu.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <div class="mb-2">
+      <Link class="flex items-center group py-2" :href="route('dashboard')">
+        <icon
+          name="dashboard"
+          class="w-4 h-4 mr-2"
+          :class="
+            isUrl('') ? 'fill-white' : 'fill-indigo-400 group-hover:fill-white'
+          "
+        />
+        <div
+          :class="
+            isUrl('') ? 'text-white' : 'text-indigo-300 group-hover:text-white'
+          "
+        >
+          Dashboard
+        </div>
+      </Link>
+    </div>
+    <!-- <div class="mb-2">
+      <Link class="flex items-center group py-2" :href="route('users')">
+        <icon
+          name="users"
+          class="w-4 h-4 mr-2"
+          :class="
+            isUrl('users')
+              ? 'fill-white'
+              : 'fill-indigo-400 group-hover:fill-white'
+          "
+        />
+        <div
+          :class="
+            isUrl('users')
+              ? 'text-white'
+              : 'text-indigo-300 group-hover:text-white'
+          "
+        >
+          Users
+        </div>
+      </Link>
+    </div> -->
+  </div>
+</template>
+
+<script>
+import Icon from "@/Components/Icon";
+import { Link } from "@inertiajs/inertia-vue3";
+
+export default {
+  components: {
+    Icon,
+    Link,
+  },
+  methods: {
+    isUrl(...urls) {
+      let currentUrl = this.$page.url.substr(1);
+      if (urls[0] === "") {
+        return currentUrl === "";
+      }
+      return urls.filter((url) => currentUrl.startsWith(url)).length;
+    },
+  },
+};
+</script>
---
+++ resources/js/Pages/Dashboard/Index.vue
@@ -0,0 +1,23 @@
+<template>
+  <Head title="Dashboard" />
+
+  <div>
+    <h1 class="mb-8 font-bold text-2xl">Dashboard</h1>
+    <p class="mb-8 leading-normal">
+      Hey there! Welcome to Laravel Inertia Starter Kit.
+    </p>
+  </div>
+</template>
+
+<script>
+import Layout from "@/Layouts/Layout";
+import { Head } from "@inertiajs/inertia-vue3";
+
+export default {
+  metaInfo: { title: "Dashboard" },
+  components: {
+    Head,
+  },
+  layout: Layout,
+};
+</script>
